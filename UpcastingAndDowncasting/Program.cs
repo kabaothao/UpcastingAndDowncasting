a79369cd53c88a524a0747e17aa05081ea5b7905
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-04 11:46:53 -0600

    updated comments

--- UpcastingAndDowncasting/Program.cs
+++ UpcastingAndDowncasting/Program.cs
@@ -1,17 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace UpcastingAndDowncasting // Note: actual namespace depends on the project name.
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        public static class Program
+            {
+                static void Main(string[] args)
+                {
+                    //Text text = new Text();
+                    //Shape shape = text;
+
+                    //text.Width = 200;
+                    //shape.Width = 100;
+
+                    //Console.WriteLine(text.Width);
+
+                    StreamReader reader =  new StreamReader(args[0]); //note that here we have a few overloads or the constructor of this class. In the framework stream is the base or parent of every kind of a stream.
+
+
         }
-    }
+            }
 }
 
 
@@ -54,6 +64,28 @@ if (obj is Car)
     Car car = (Car) obj;
 ...
 }
+
+
+
+
+Both this text and shape objects are actually pointing to the same object in memory.
+
+Well it's more accurate to say takes and shape are both references to the same object in memory but
+
+they have different views what it means is if I go here and say text dot I can see all the properties
+
+of a Takes object.
+
+
+
+Line 18
+Because all text and shape are references to the same object.
+
+They just have different views.
+
+
+
+
 
 
  */
